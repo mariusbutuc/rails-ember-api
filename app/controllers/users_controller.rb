b34db08675a1a9d9e6549e986239f5599f8f576c
Marius Butuc <marius.butuc@gmail.com>
2013-02-14 17:28:08 -0500

    Hehe, handwritten controllers...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # in a RailsAPI app ApplicationController itself inherits from
 # ActionController::API instead of ActionController::Base.
 
-class UserController < ApplicationController
+class UsersController < ApplicationController
   def index
     render json: User.all
   end
